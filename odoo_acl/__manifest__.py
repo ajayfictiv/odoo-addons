69200616ee6ed4d9562bd14fed60e0ec51c8b28b
Ajay Chauhan <ajay.chauhan@fictiv.com>
2020-04-27 17:57:49 +0530

    [ADD]:Added init and menifest files for module odoo_acl.

---
+++ odoo_acl/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo ACL",
+
+    'summary': """
+        Demo module to show odoo access controls""",
+
+    'description': """
+        This module will cover following.
+            > Access Rights on models per group
+            > Field level access per group
+            > Records level per group
+    """,
+
+    'author': "Ajay Chauhan <ajay.chauhan@fictiv.com>",
+    'website': "http://www.fictiv.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    #UPDATEME remove dependancy of fictiv_digital_model
+    'depends': []
+
+    # always loaded
+    'data': []
+
+}
