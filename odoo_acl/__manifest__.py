d2eeceeeefe34954e4dc982229687073c1d5a5d0
Ajay Chauhan <ajay.chauhan@fictiv.com>
2020-04-29 19:45:20 +0530

    [ADD]:Added Roles adn access rights for machine object.

--- odoo_acl/__manifest__.py
+++ odoo_acl/__manifest__.py
@@ -16,17 +16,19 @@
     'website': "http://www.fictiv.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # Check https://github.com/odoo/odoo/blob/10.0/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Administration',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    #UPDATEME remove dependancy of fictiv_digital_model
+    # UPDATEME remove dependancy of fictiv_digital_model
     'depends': [],
 
     # always loaded
     'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/res_partner_views.xml',
         'views/machine_views.xml',
     ]
