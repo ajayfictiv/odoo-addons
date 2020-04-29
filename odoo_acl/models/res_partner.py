2dd927b23ab6c732024a69cfd3b6f832484cd609
Ajay Chauhan <ajay.chauhan@fictiv.com>
2020-04-29 20:14:08 +0530

    [ADD]:Added calcualted field total machine value in vendor.

--- odoo_acl/models/res_partner.py
+++ odoo_acl/models/res_partner.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ResPartner(models.Model):
@@ -8,3 +8,11 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     machine_ids = fields.One2many('machine', 'vendor_id')
+    total_machine_value = fields.Float(
+        compute='calculate_machine_value', store=True)
+
+    @api.depends('machine_ids', 'machine_ids.cost')
+    def calculate_machine_value(self):
+        for partner in self:
+            partner.total_machine_value = sum(
+                partner.machine_ids.mapped('cost'))
